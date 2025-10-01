ce77f2686a78eeb7a06aa282b616ad29050efa40
foxcaulfield <fox.caulfield@gmail.com>
2025-10-01 11:58:59 +0200

    Wipe structure

--- src/reservations/dto/create-reservation.dto.ts
+++
@@ -1,9 +0,0 @@
-import { IsDate, IsMongoId } from "class-validator";
-
-export class CreateReservationDto {
-	@IsMongoId()
-	public room!: string;
-
-	@IsDate()
-	public reservationDate!: Date;
-}
--- src/reservations/dto/response-reservation.dto.ts
+++
@@ -1,9 +0,0 @@
-import { IsDate, IsMongoId } from "class-validator";
-
-export class ResponseReservationDto {
-	@IsMongoId()
-	public room!: string;
-
-	@IsDate()
-	public reservationDate!: Date;
-}
